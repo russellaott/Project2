0cb5ed48ac452b5c73d45678d276f7a5c74bb84e
Russell Ott <russellaott@gmail.com>
2018-09-20 23:17:32 -0500

    updated connection file, modified database tables, updated seed file

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,2 +1,16 @@
-INSERT INTO trip (departCity, departState, destinationCity, destinationState, dt, smoking, details)
-VALUES ('Dallas', 'Texas', 'Destin', 'Florida', '2019-07-04' , TRUE , 'Looking for someone to ride straight through to Destin with me.');
+INSERT INTO trip (seatsAvailable, departCity, departState, destinationCity, destinationState, dt, smoking, details)
+VALUES ('4', 'Dallas', 'Texas', 'Destin', 'Florida', '2019-07-04' , TRUE , 'Looking for someone to ride straight through to Destin with me.');
+INSERT INTO trip (seatsAvailable, departCity, departState, destinationCity, destinationState, dt, smoking, details)
+VALUES ('1', 'Austin', 'Texas', 'Atlanta', 'Georgia', '2019-05-04' , TRUE , 'Need one person to join me to Atlanta.');
+INSERT INTO trip (seatsAvailable, departCity, departState, destinationCity, destinationState, dt, smoking, details)
+VALUES ('5', 'Portland', 'Oregon', 'Los Angeles', 'California', '2019-01-06' , TRUE , 'Moving to Cali and looking for a group to ride along.');
+INSERT INTO trip (seatsAvailable, departCity, departState, destinationCity, destinationState, dt, smoking, details)
+VALUES ('2', 'Chicago', 'Illinois', 'New York', 'New York', '2019-02-02' , FALSE , 'Roadtrip to visit family. 2 spots left in my Civic if anyone wants to tag along.');
+INSERT INTO trip (seatsAvailable, departCity, departState, destinationCity, destinationState, dt, smoking, details)
+VALUES ('2', 'Seattle', 'Washington', 'Miami', 'Florida', '2019-03-012' , TRUE , 'Spring Break in Miami!');
+INSERT INTO trip (seatsAvailable, departCity, departState, destinationCity, destinationState, dt, smoking, details)
+VALUES ('1', 'Dallas', 'Texas', 'Oklahoma City', 'Oklahoma', '2018-12-12' , FALSE , 'Looking for someone to ride and split expenses.');
+INSERT INTO trip (seatsAvailable, departCity, departState, destinationCity, destinationState, dt, smoking, details)
+VALUES ('1', 'San Francisco', 'California', 'Los Angeles', 'California', '2019-01-01' , FALSE , 'Visit friends in LA.');
+INSERT INTO trip (seatsAvailable, departCity, departState, destinationCity, destinationState, dt, smoking, details)
+VALUES ('2', 'Houston', 'Texas', 'Denver', 'Colorado', '2019-07-31' , TRUE , 'Going to live the high life for a week :).');
